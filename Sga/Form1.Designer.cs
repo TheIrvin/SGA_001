cd9253ec89c7c1ee2bdf4ba4eda902d8a304603a
TheIrvin <irvincajas72@gmail.com>
2025-02-24 19:57:50 -0500

    Revert "Rama buena2.0"

--- Sga/Form1.Designer.cs
+++ Sga/Form1.Designer.cs
@@ -50,10 +50,9 @@
             this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
             this.lblTitulo.Font = new System.Drawing.Font("Goudy Old Style", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.lblTitulo.Location = new System.Drawing.Point(247, 9);
-            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Location = new System.Drawing.Point(186, 9);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(106, 51);
+            this.lblTitulo.Size = new System.Drawing.Size(85, 41);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "SGA";
             this.lblTitulo.Click += new System.EventHandler(this.lblTitulo_Click);
@@ -63,10 +62,9 @@
             this.lbllUsu.AutoSize = true;
             this.lbllUsu.BackColor = System.Drawing.Color.Transparent;
             this.lbllUsu.ForeColor = System.Drawing.Color.Blue;
-            this.lbllUsu.Location = new System.Drawing.Point(93, 181);
-            this.lbllUsu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbllUsu.Location = new System.Drawing.Point(70, 147);
             this.lbllUsu.Name = "lbllUsu";
-            this.lbllUsu.Size = new System.Drawing.Size(364, 16);
+            this.lbllUsu.Size = new System.Drawing.Size(313, 13);
             this.lbllUsu.TabIndex = 1;
             this.lbllUsu.Text = "___________________________________________________";
             // 
@@ -75,41 +73,52 @@
             this.lbllContra.AutoSize = true;
             this.lbllContra.BackColor = System.Drawing.Color.Transparent;
             this.lbllContra.ForeColor = System.Drawing.Color.Blue;
-            this.lbllContra.Location = new System.Drawing.Point(93, 247);
-            this.lbllContra.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbllContra.Location = new System.Drawing.Point(70, 201);
             this.lbllContra.Name = "lbllContra";
-            this.lbllContra.Size = new System.Drawing.Size(364, 16);
+            this.lbllContra.Size = new System.Drawing.Size(313, 13);
             this.lbllContra.TabIndex = 2;
             this.lbllContra.Text = "___________________________________________________";
             // 
-// txtBox_Usuario_login
-// 
-this.txtBox_Usuario_login.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-this.txtBox_Usuario_login.BorderStyle = System.Windows.Forms.BorderStyle.None;
-this.txtBox_Usuario_login.Font = new System.Drawing.Font("Century Gothic", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-this.txtBox_Usuario_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-this.txtBox_Usuario_login.Location = new System.Drawing.Point(71, 140);
-this.txtBox_Usuario_login.Name = "txtBox_Usuario_login";
-this.txtBox_Usuario_login.Size = new System.Drawing.Size(310, 19);
-this.txtBox_Usuario_login.TabIndex = 2;
-this.txtBox_Usuario_login.Text = "USUARIO";
-this.txtBox_Usuario_login.TextChanged += new System.EventHandler(this.txtUsu_TextChanged);
-this.txtBox_Usuario_login.Enter += new System.EventHandler(this.txtUsu_Enter);
-this.txtBox_Usuario_login.Leave += new System.EventHandler(this.txtUsu_Leave);
-this.txtBox_Usuario_login.MouseEnter += new System.EventHandler(this.txtUsu_MouseEnter);
-this.txtBox_Usuario_login.MouseLeave += new System.EventHandler(this.txtUsu_MouseLeave);
-this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_MouseHover);
-
+            // txtBox_Usuario_login
+            // 
+            this.txtBox_Usuario_login.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBox_Usuario_login.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_Usuario_login.Font = new System.Drawing.Font("Century Gothic", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_Usuario_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.txtBox_Usuario_login.Location = new System.Drawing.Point(71, 140);
+            this.txtBox_Usuario_login.Name = "txtBox_Usuario_login";
+            this.txtBox_Usuario_login.Size = new System.Drawing.Size(310, 19);
+            this.txtBox_Usuario_login.TabIndex = 2;
+            this.txtBox_Usuario_login.Text = "USUARIO";
+            this.txtBox_Usuario_login.TextChanged += new System.EventHandler(this.txtUsu_TextChanged);
+            this.txtBox_Usuario_login.Enter += new System.EventHandler(this.txtUsu_Enter);
+            this.txtBox_Usuario_login.Leave += new System.EventHandler(this.txtUsu_Leave);
+            this.txtBox_Usuario_login.MouseEnter += new System.EventHandler(this.txtUsu_MouseEnter);
+            this.txtBox_Usuario_login.MouseLeave += new System.EventHandler(this.txtUsu_MouseLeave);
+            this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_MouseHover);
+            // 
+            // txtBox_Contraseña_login
+            // 
+            this.txtBox_Contraseña_login.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBox_Contraseña_login.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_Contraseña_login.Font = new System.Drawing.Font("Century Gothic", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_Contraseña_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.txtBox_Contraseña_login.Location = new System.Drawing.Point(73, 192);
+            this.txtBox_Contraseña_login.Name = "txtBox_Contraseña_login";
+            this.txtBox_Contraseña_login.Size = new System.Drawing.Size(309, 19);
+            this.txtBox_Contraseña_login.TabIndex = 3;
+            this.txtBox_Contraseña_login.Text = "CONTRASEÑA";
+            this.txtBox_Contraseña_login.Enter += new System.EventHandler(this.txtContra_Enter);
+            this.txtBox_Contraseña_login.Leave += new System.EventHandler(this.txtContra_Leave);
             // 
             // btnAcceder
             // 
             this.btnAcceder.BackColor = System.Drawing.Color.Transparent;
             this.btnAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAcceder.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAcceder.Location = new System.Drawing.Point(97, 294);
-            this.btnAcceder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAcceder.Location = new System.Drawing.Point(73, 239);
             this.btnAcceder.Name = "btnAcceder";
-            this.btnAcceder.Size = new System.Drawing.Size(411, 44);
+            this.btnAcceder.Size = new System.Drawing.Size(308, 36);
             this.btnAcceder.TabIndex = 4;
             this.btnAcceder.Text = "ACCEDER";
             this.btnAcceder.UseVisualStyleBackColor = false;
@@ -121,10 +130,9 @@ this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_Mous
             this.pcCerrar.ErrorImage = null;
             this.pcCerrar.Image = ((System.Drawing.Image)(resources.GetObject("pcCerrar.Image")));
             this.pcCerrar.InitialImage = null;
-            this.pcCerrar.Location = new System.Drawing.Point(557, -1);
-            this.pcCerrar.Margin = new System.Windows.Forms.Padding(4);
+            this.pcCerrar.Location = new System.Drawing.Point(418, -1);
             this.pcCerrar.Name = "pcCerrar";
-            this.pcCerrar.Size = new System.Drawing.Size(36, 32);
+            this.pcCerrar.Size = new System.Drawing.Size(27, 26);
             this.pcCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pcCerrar.TabIndex = 6;
             this.pcCerrar.TabStop = false;
@@ -136,10 +144,9 @@ this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_Mous
             this.pictureBox1.ErrorImage = null;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(520, -1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(390, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(36, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(27, 26);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -148,12 +155,10 @@ this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_Mous
             // lblPerdida
             // 
             this.lblPerdida.AutoSize = true;
-
             this.lblPerdida.Location = new System.Drawing.Point(196, 289);
             this.lblPerdida.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.lblPerdida.Name = "lblPerdida";
-            this.lblPerdida.Size = new System.Drawing.Size(185, 16);
+            this.lblPerdida.Size = new System.Drawing.Size(149, 13);
             this.lblPerdida.TabIndex = 1;
             this.lblPerdida.TabStop = true;
             this.lblPerdida.Text = "¿Has olvidado tu contraseña?";
@@ -161,11 +166,9 @@ this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_Mous
             // lbl_Registrarse
             // 
             this.lbl_Registrarse.AutoSize = true;
-
             this.lbl_Registrarse.Location = new System.Drawing.Point(112, 289);
-
             this.lbl_Registrarse.Name = "lbl_Registrarse";
-            this.lbl_Registrarse.Size = new System.Drawing.Size(77, 16);
+            this.lbl_Registrarse.Size = new System.Drawing.Size(60, 13);
             this.lbl_Registrarse.TabIndex = 8;
             this.lbl_Registrarse.Text = "Registrarse";
             this.lbl_Registrarse.Click += new System.EventHandler(this.lbl_Registrarse_Click);
@@ -175,21 +178,19 @@ this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_Mous
             // lbl_o
             // 
             this.lbl_o.AutoSize = true;
-
             this.lbl_o.Location = new System.Drawing.Point(178, 289);
-
             this.lbl_o.Name = "lbl_o";
-            this.lbl_o.Size = new System.Drawing.Size(15, 16);
+            this.lbl_o.Size = new System.Drawing.Size(13, 13);
             this.lbl_o.TabIndex = 9;
             this.lbl_o.Text = "o";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(592, 383);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(444, 311);
             this.Controls.Add(this.lbl_o);
             this.Controls.Add(this.lbl_Registrarse);
             this.Controls.Add(this.lblPerdida);
@@ -203,7 +204,6 @@ this.txtBox_Usuario_login.MouseHover += new System.EventHandler(this.txtUsu_Mous
             this.Controls.Add(this.lblTitulo);
             this.ForeColor = System.Drawing.Color.Blue;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
